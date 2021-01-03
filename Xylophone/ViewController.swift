74cff40b97faa3e672ae31d81b29efa77423eb52
rihab <rihbalyasiri@yahoo.com>
2021-01-03 10:49:10 +0100

    adding sounds to xylophone

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,32 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        print("start")
+        sender.alpha = 0.5
+        playSound(sound: sender.currentTitle)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+           // Bring's sender's opacity back up to fully opaque
+           sender.alpha = 1.0
+            print("end")
+        }
+    }
     
-
+    func playSound(sound: String?) {
+        let url = Bundle.main.url(forResource: sound, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
 
